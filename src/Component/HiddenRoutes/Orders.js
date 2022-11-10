e21f22d217ca071106a75ce309504f826b609bdf
Ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-10 15:01:20 +0600

    order done

--- src/Component/HiddenRoutes/Orders.js
+++ src/Component/HiddenRoutes/Orders.js
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Context/AuthProvider/AuthProvider';
+import Extra from '../Extra';
+
+
 
 const Orders = () => {
+
+    const {user} = useContext(AuthContext);
+
+    const [orders, setOrders] = useState([])
+
+    
+    
+    useEffect(() =>{
+
+        fetch(`http://localhost:5000/orders?email=${user.email}`)
+        .then(res => res.json())
+        .then(data => setOrders(data))
+
+    },[user?.email])
+
+
+
     return (
         <div>
-             orders area
+            <h2 className='text-2xl font-bold text-error mb-5'> YOUR HAVE : 0{orders.length} orders.</h2>
+            
+            {
+                orders.map(order => <Extra 
+                    
+                    key={orders._id}
+                    order={order}
+                    ></Extra>)
+            }
+
         </div>
     );
 };
