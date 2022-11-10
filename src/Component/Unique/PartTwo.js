c247c5c9b97be6e608b64190d058e7c538f0325d
Ronyhassansam03n <ronyhassansam03n@gmail.com>
2022-11-10 17:39:02 +0600

    unique done

--- src/Component/Unique/PartTwo.js
+++ src/Component/Unique/PartTwo.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import picc from '../../assets/unique/part2.jpg'
 
 const PartTwo = () => {
     return (
         <div>
-            part 2
+     <div className="card lg:card-side bg-base-100 shadow-xl w-3/4 mt-10 mx-auto mb-10">
+                <figure><img src={picc} alt="Album" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title font-bold text-2xl">Welcome to Creative Photography</h2>
+                    <p> One of the most committed, artistic, honest & experienced wedding photographers in the northern Indiana area, there's no better investment than Creative Photography.
+                    </p>
+                    <Link to='/services'> <button className="btn btn-wide btn-primary">OUR SERVICES</button></Link>
+                </div>
+            </div>
+
         </div>
     );
 };
